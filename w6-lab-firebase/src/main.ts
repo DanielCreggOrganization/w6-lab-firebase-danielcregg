0476c8dc2b1298669cd33999a643da1019c8c751
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-10-20 22:27:01 +0000

    Initialize Firebase project structure and add basic app components

---
+++ w6-lab-firebase/src/main.ts
@@ -0,0 +1,29 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "fir-ionic-project-dc52e",
+      appId: "1:769063383414:web:0b402d09efd31d324dca57",
+      storageBucket: "fir-ionic-project-dc52e.appspot.com",
+      apiKey: "AIzaSyDibno0p2mUnQmjN6RlfXlHjbguzSIUjFY",
+      authDomain: "fir-ionic-project-dc52e.firebaseapp.com",
+      messagingSenderId: "769063383414"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideVertexAI(() => getVertexAI()),
+  ],
+});
