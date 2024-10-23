675f0c10c83fb099e28f57af6b4ab7ee12bcd5eb
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-10-23 17:40:20 +0000

    Refactor home page HTML: improve task display logic, enhance modal functionality, and streamline button actions

--- w6-lab-firebase/src/app/home/home.page.ts
+++ w6-lab-firebase/src/app/home/home.page.ts
@@ -1,3 +1,6 @@
+/**
+ * HomePage Component for managing tasks
+ */
 import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -63,27 +66,40 @@ import { TasksService, Task } from '../tasks.service';
     IonFabButton,
   ],
 })
-
 export class HomePage implements AfterViewInit {
-  newTask!: Task; // This is the task that will be added to the database.
-  @ViewChild(IonModal) modal!: IonModal; // Find the first IonModal in my template and assign it to the modal property of my class.
-  tasks$!: Observable<Task[]>; // This is an observable that will emit the current value of the tasks array. // This is an observable that will emit the current value of the tasks array.
+  /**
+   * The new task being created
+   */
+  newTask: Task = {
+    content: '',
+    completed: false
+  };
 
-  private authService = inject(AuthService);
-  private tasksService = inject(TasksService);
-  private router = inject(Router);
-  private loadingController = inject(LoadingController);
-  private alertController = inject(AlertController);
+  /**
+   * Reference to the modal in the template
+   */
+  @ViewChild(IonModal) modal!: IonModal;
+  
+  /**
+   * Observable of user tasks
+   */
+  userTasks$: Observable<Task[]>;
 
-  constructor() { }
+  private readonly authService = inject(AuthService);
+  private readonly tasksService = inject(TasksService);
+  private readonly router = inject(Router);
+  private readonly loadingController = inject(LoadingController);
+  private readonly alertController = inject(AlertController);
 
-  ngOnInit() {
-    this.resetTask();
-    addIcons({ logOutOutline, pencilOutline, trashOutline, add });
-    this.tasks$ = this.tasksService.readTasks();
+  constructor() {
+    // Initialize the tasks observable
+    this.userTasks$ = this.tasksService.getUserTasks();
   }
 
-  // This method is used to reset the newTask property. This will clear the input in the modal.
+  ngOnInit() {
+    addIcons({ logOutOutline, pencilOutline, trashOutline, add });
+  }
+
   resetTask() {
     this.newTask = {
       content: '',
@@ -91,39 +107,48 @@ export class HomePage implements AfterViewInit {
     };
   }
 
-  // This method is used to log the user out. The button will be found in the top right corner of the home page.
   async logout() {
-    // Call the logout method in the auth service. Use await to wait for the logout to complete before continuing.
     await this.authService.logout();
-    // Navigate to the login page with the replaceUrl option.
-    // This means that the login page will replace the home page in the navigation stack.
     this.router.navigateByUrl('/', { replaceUrl: true });
   }
 
-  // This method is used to add a task to the database
   async addTask() {
-    const loading = await this.loadingController.create();
-    // await means that the code will wait for the loading to be presented before continuing
-    await loading.present();
-    // Add the task to the database
-    this.tasksService.createTask(this.newTask);
-    // Dismiss the loading
-    await loading.dismiss();
-    // Dismiss the modal
-    this.modal.dismiss(null, 'confirm');
-    // Reset the task. This will clear the input in the modal.
-    this.resetTask();
+    try {
+      const loading = await this.loadingController.create({
+        message: 'Adding task...'
+      });
+      await loading.present();
+      
+      await this.tasksService.createTask(this.newTask);
+      
+      await loading.dismiss();
+      this.modal.dismiss(null, 'confirm');
+      this.resetTask();
+    } catch (error) {
+      console.error('Error adding task:', error);
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Failed to add task. Please try again.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
 
-  // This method is used to update the checkbox in the UI when the user toggles the checkbox
   async toggleTask(ionCheckboxEvent: Event, task: Task) {
-    task.completed = (ionCheckboxEvent as CheckboxCustomEvent).detail.checked;
-    await this.tasksService.toggleTaskCompleted(task);
-  }
-
-  async updateTask() {
-    await this.tasksService.updateTask(this.newTask);
-    this.resetTask();
+    try {
+      task.completed = (ionCheckboxEvent as CheckboxCustomEvent).detail.checked;
+      await this.tasksService.toggleTaskCompleted(task);
+    } catch (error) {
+      console.error('Error toggling task:', error);
+      task.completed = !task.completed;
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Failed to update task status. Please try again.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
 
   async openUpdateInput(task: Task) {
@@ -144,43 +169,58 @@ export class HomePage implements AfterViewInit {
         },
         {
           text: 'Update',
-          // Call the updateTask method when the user clicks the update button
-          handler: (data) => {
-            task.content = data.Task;
-            this.tasksService.updateTask(task);
+          handler: async (data) => {
+            try {
+              task.content = data.Task;
+              await this.tasksService.updateTask(task);
+              return true; // Close the alert on success
+            } catch (error) {
+              console.error('Error updating task:', error);
+              const errorAlert = await this.alertController.create({
+                header: 'Error',
+                message: 'Failed to update task. Please try again.',
+                buttons: ['OK']
+              });
+              await errorAlert.present();
+              return false; // Keep the alert open on error
+            }
           },
         },
       ],
     });
-    await alert.present(); // Present the alert to the user
-    // Get the alert's first input element and focus the mouse blinker on it.
-    // The setTimeout function is used to allow some time for the browser to render the alert's DOM elements.
+    
+    await alert.present();
+    
     setTimeout(() => {
-      const firstInput: any = document.querySelector('ion-alert input');
-      firstInput.focus();
+      const firstInput: HTMLInputElement | null = document.querySelector('ion-alert input');
+      firstInput?.focus();
     }, 250);
   }
 
-  deleteTask(task: Task) {
-    // Print task to console
-    console.log('Deleting task: ', task);
-    this.tasksService.deleteTask(task);
+  async deleteTask(task: Task) {
+    try {
+      await this.tasksService.deleteTask(task);
+    } catch (error) {
+      console.error('Error deleting task:', error);
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Failed to delete task. Please try again.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
 
-  // The method is used inside the modal to close the modal and reset the newTask property.
   cancel() {
     this.modal.dismiss(null, 'cancel');
     this.resetTask();
   }
 
-  // This method is used to focus the cursor in the input box of the modal when we open it. We subscribe to
-  // the ionModalDidPresent event of the modal. When the modal is presented, we use setTimeout to wait for
-  // the browser to render the modal's DOM elements, then we select the first input element in the modal and focus on it.
   ngAfterViewInit() {
     this.modal.ionModalDidPresent.subscribe(() => {
       setTimeout(() => {
-        const firstInput: any = document.querySelector('ion-modal input');
-        firstInput.focus();
+        const firstInput: HTMLInputElement | null = document.querySelector('ion-modal input');
+        firstInput?.focus();
       }, 250);
     });
   }
