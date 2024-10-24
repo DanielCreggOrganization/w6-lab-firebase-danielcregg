d20b1fc2f7746e9784184e333d6431ff3b0ae30a
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-10-24 10:19:58 +0000

    Refactor main.ts: remove unused VertexAI provider to streamline dependencies

--- w6-lab-firebase/src/main.ts
+++ w6-lab-firebase/src/main.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app/app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -24,6 +23,5 @@ bootstrapApplication(AppComponent, {
     })),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideVertexAI(() => getVertexAI()),
   ],
 });
